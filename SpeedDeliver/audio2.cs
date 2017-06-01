f8b7375625fec5aa1a8521601c3d660763ac7788
zhoushuo1 <1225463803@qq.com>
2017-06-01 08:37:39 +0800

    音乐音效脚本

---
+++ SpeedDeliver/audio2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class audio2 : MonoBehaviour
+{
+    public AudioSource music;
+    public Toggle toggle;
+    // Use this for initialization
+    void Start()
+    {
+        toggle.onValueChanged.AddListener(OnValueChanged); 
+    }
+    void OnValueChanged(bool check)
+    {
+        if (!music.isPlaying)
+        {
+            music.Play();
+        }
+        else
+        {
+            music.Stop();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

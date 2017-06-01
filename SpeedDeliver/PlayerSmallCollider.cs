39fab75b6af86fffb23a0d2fd425203127ae5031
zhoushuo1 <1225463803@qq.com>
2017-06-01 08:21:36 +0800

    人物

---
+++ SpeedDeliver/PlayerSmallCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSmallCollider : MonoBehaviour {
+
+    private PlayerMove playerMove;
+    void Awake() {
+        playerMove = GameObject.FindGameObjectWithTag(Tags.player).GetComponent<PlayerMove>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == Tags.obstacles && playerMove.isSliding) {
+            GameController.gameState = GameState.End;
+        }
+    }
+
+}
